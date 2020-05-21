a8ac364a5fd3b352c22d3e6bb53687f77f8626e4
SakshiSinha <sakshisinha202326@gmail.com>
2020-05-21 15:04:58 +0530

    project

---
+++ build/precache-manifest.a642fc2afa10cb3c24eab441f990487d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaaa2f3e05a6485e67dfd43c41159f3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d2b8bea473fa4c249f3",
+    "url": "/static/css/main.99e992d9.chunk.css"
+  },
+  {
+    "revision": "e9aa4844797de3788072",
+    "url": "/static/js/2.10340061.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.10340061.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d2b8bea473fa4c249f3",
+    "url": "/static/js/main.1f3d9833.chunk.js"
+  },
+  {
+    "revision": "a780b1ce2049a0279d24",
+    "url": "/static/js/runtime-main.5ff3a1c8.js"
+  }
+]);
